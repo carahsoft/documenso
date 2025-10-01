fe2ad15cc1a009c52778ed5990da70ae10ddbc3d
Ryan Wagoner <Ryan.Wagoner@carahsoft.com>
2025-10-01 16:22:51 -0400

    feat: add option to allow only admins to create organisations and teams

--- apps/remix/app/routes/_authenticated+/settings+/organisations.tsx
+++ apps/remix/app/routes/_authenticated+/settings+/organisations.tsx
@@ -1,6 +1,10 @@
 import { msg } from '@lingui/core/macro';
 import { useLingui } from '@lingui/react';
 
+import { useSession } from '@documenso/lib/client-only/providers/session';
+import { ADMIN_CREATE_ORGANISATION_ENABLED } from '@documenso/lib/constants/app';
+import { isAdmin } from '@documenso/lib/utils/is-admin';
+
 import { OrganisationCreateDialog } from '~/components/dialogs/organisation-create-dialog';
 import { OrganisationInvitations } from '~/components/general/organisations/organisation-invitations';
 import { SettingsHeader } from '~/components/general/settings-header';
@@ -8,6 +12,9 @@ import { UserOrganisationsTable } from '~/components/tables/user-organisations-t
 
 export default function TeamsSettingsPage() {
   const { _ } = useLingui();
+  const { user } = useSession();
+  const isUserAdmin = isAdmin(user);
+  const canCreateOrganisation = !ADMIN_CREATE_ORGANISATION_ENABLED() || isUserAdmin;
 
   return (
     <div>
@@ -15,7 +22,7 @@ export default function TeamsSettingsPage() {
         title={_(msg`Organisations`)}
         subtitle={_(msg`Manage all organisations you are currently associated with.`)}
       >
-        <OrganisationCreateDialog />
+        {canCreateOrganisation && <OrganisationCreateDialog />}
       </SettingsHeader>
 
       <UserOrganisationsTable />
