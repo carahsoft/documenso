86c708d1ddfbf626e29059ee73d9dafdb9fe8eb0
Ryan Wagoner <Ryan.Wagoner@carahsoft.com>
2025-10-02 15:23:52 -0400

    fix: update react email for outlook compatibility

--- packages/email/render.tsx
+++ packages/email/render.tsx
@@ -1,3 +1,5 @@
+import type { I18n } from '@lingui/core';
+import { I18nProvider } from '@lingui/react';
 import * as ReactEmail from '@react-email/render';
 
 import config from '@documenso/tailwind-config';
@@ -7,45 +9,64 @@ import { BrandingProvider, type BrandingSettings } from './providers/branding';
 
 export type RenderOptions = ReactEmail.Options & {
   branding?: BrandingSettings;
+  i18n?: I18n;
 };
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
 const colors = (config.theme?.extend?.colors || {}) as Record<string, string>;
 
-export const render = (element: React.ReactNode, options?: RenderOptions) => {
-  const { branding, ...otherOptions } = options ?? {};
+export const render = async (element: React.ReactNode, options?: RenderOptions) => {
+  const { branding, i18n, ...otherOptions } = options ?? {};
 
-  return ReactEmail.render(
-    <Tailwind
-      config={{
-        theme: {
-          extend: {
-            colors,
+  const wrappedElement = (
+    <BrandingProvider branding={branding}>
+      <Tailwind
+        config={{
+          theme: {
+            extend: {
+              colors,
+            },
           },
-        },
-      }}
-    >
-      <BrandingProvider branding={branding}>{element}</BrandingProvider>
-    </Tailwind>,
-    otherOptions,
+        }}
+      >
+        {element}
+      </Tailwind>
+    </BrandingProvider>
   );
+
+  const finalElement = i18n ? (
+    <I18nProvider i18n={i18n}>{wrappedElement}</I18nProvider>
+  ) : (
+    wrappedElement
+  );
+
+  return ReactEmail.render(finalElement, otherOptions);
 };
 
 export const renderAsync = async (element: React.ReactNode, options?: RenderOptions) => {
-  const { branding, ...otherOptions } = options ?? {};
+  const { branding, i18n, ...otherOptions } = options ?? {};
 
-  return await ReactEmail.renderAsync(
-    <Tailwind
-      config={{
-        theme: {
-          extend: {
-            colors,
+  const wrappedElement = (
+    <BrandingProvider branding={branding}>
+      <Tailwind
+        config={{
+          theme: {
+            extend: {
+              colors,
+            },
           },
-        },
-      }}
-    >
-      <BrandingProvider branding={branding}>{element}</BrandingProvider>
-    </Tailwind>,
-    otherOptions,
+        }}
+      >
+        {element}
+      </Tailwind>
+    </BrandingProvider>
   );
+
+  const finalElement = i18n ? (
+    <I18nProvider i18n={i18n}>{wrappedElement}</I18nProvider>
+  ) : (
+    wrappedElement
+  );
+
+  return await ReactEmail.renderAsync(finalElement, otherOptions);
 };
