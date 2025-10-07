d362feaeb5414509e0729f8894c8007309cf26db
Ryan Wagoner <Ryan.Wagoner@carahsoft.com>
2025-10-07 19:52:07 -0400

    chore: remove branding from signed pdf

--- packages/signing/helpers/add-signing-placeholder.ts
+++ packages/signing/helpers/add-signing-placeholder.ts
@@ -33,7 +33,6 @@ export const addSigningPlaceholder = async ({ pdf }: AddSigningPlaceholderOption
       SubFilter: 'adbe.pkcs7.detached',
       ByteRange: byteRange,
       Contents: PDFHexString.fromText(' '.repeat(8192)),
-      Reason: PDFString.of('Signed by Documenso'),
       M: PDFString.fromDate(new Date()),
     }),
   );
