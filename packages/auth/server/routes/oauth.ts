47e6a9702450650eb87a83ad88141ecd9d47d065
Ryan Wagoner <Ryan.Wagoner@carahsoft.com>
2025-10-08 08:28:53 -0400

    feat: microsoft azure entra id sso

--- packages/auth/server/routes/oauth.ts
+++ packages/auth/server/routes/oauth.ts
@@ -2,7 +2,7 @@ import { sValidator } from '@hono/standard-validator';
 import { Hono } from 'hono';
 import { z } from 'zod';
 
-import { GoogleAuthOptions, OidcAuthOptions } from '../config';
+import { EntraAuthOptions, GoogleAuthOptions, OidcAuthOptions } from '../config';
 import { handleOAuthAuthorizeUrl } from '../lib/utils/handle-oauth-authorize-url';
 import { getOrganisationAuthenticationPortalOptions } from '../lib/utils/organisation-portal';
 import type { HonoAuthContext } from '../types/context';
@@ -22,6 +22,19 @@ export const oauthRoute = new Hono<HonoAuthContext>()
       c,
       clientOptions: GoogleAuthOptions,
       redirectPath,
+    });
+  })
+  /**
+   * Microsoft Entra ID authorize endpoint.
+   */
+  .post('/authorize/entra', sValidator('json', ZOAuthAuthorizeSchema), async (c) => {
+    const { redirectPath } = c.req.valid('json');
+
+    return handleOAuthAuthorizeUrl({
+      c,
+      clientOptions: EntraAuthOptions,
+      redirectPath,
+      prompt: 'select_account',
     });
   })
   /**
